2f27b6c33b6774a26bc6e62f43bc9d9e3340454e
Animesh Shukla <animeshshukla1518@gmail.com>
2024-08-19 20:14:50 +0530

    feat: added newsletter sending

--- bot.js
+++ bot.js
@@ -1,11 +1,15 @@
 const TelegramBot = require('node-telegram-bot-api');
 const dotenv = require('dotenv');
+const axios = require('axios');
+const { enc_data } = require('./config');
 
 dotenv.config();
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const admins = ['animeshshukla01'];
+
+
 
 const bot = new TelegramBot(token, { polling: true });
 
@@ -28,21 +32,21 @@ const menu = {
     }
 };
 
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
     const chatID = msg.chat.id;
     const messageText = msg.text;
 
     if (messageText === "/start") {
-        bot.sendMessage(chatID, 'Hey there! I am Brook, your personal assistant. How can I help you today? Hit /menu to see the menu.', menu);
+        bot.sendMessage(chatID, 'Hey there! I am Brook, your personal assistant. How can I help you today?');
     }
 
-    if(messageText === "/menu"){
+    if (messageText === "/menu") {
         bot.sendMessage(chatID, "Here is the menu", menu);
 
     }
 
 
-    if (messageText==="/admin") {
+    if (messageText === "/admin") {
         if (admins.includes(msg.from.username)) {
             bot.sendMessage(chatID, 'You are an admin');
         } else {
@@ -50,12 +54,20 @@ bot.on('message', (msg) => {
         }
     }
 
-    if(messageText === "/send-newsletter"){
-        if(!checkAdmin(msg.from.username)){
+    if (messageText === "/send-newsletter") {
+        if (!checkAdmin(msg.from.username)) {
             bot.sendMessage(chatID, "You are not authorized to send newsletters.");
             return;
         }
-        bot.sendMessage(chatID, "Please enter the title of the newsletter.");
+        bot.sendMessage(chatID, "Please wait...Authenticating user...");
+        const auth_token = await getToken(msg.from.username);
+        if (!auth_token) {
+            bot.sendMessage(chatID, "Authentication failed. Please try again later.");
+            return;
+        }
+        bot.sendMessage(chatID, "User authenticated successfully 🎉");
+
+        bot.sendMessage(chatID, "Please enter the newsletter title");
 
         bot.once('message', (msg) => {
             const newsletter_title = msg.text;
@@ -70,23 +82,33 @@ bot.on('message', (msg) => {
                 bot.once('message', (msg) => {
                     const content = msg.text;
 
-                    fetch('http://localhost:8080/misc/send-newsletter', {
+                    bot.sendMessage(chatID,"Please wait...Sending newsletter...");
+
+                    fetch('https://trustherole.in/ttrapi/misc/send-newsletter', {
                         method: 'POST',
                         headers: {
-                            'Content-Type': 'application/json'
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${auth_token}`
                         },
                         body: JSON.stringify({
-                            newsletter_title:newsletter_title,
-                            title:topic_name,
-                            content:content
+                            newsletter_title: newsletter_title,
+                            title: topic_name,
+                            content: content
                         })
-                    }).then((res)=>{
-                        res.json().then((data)=>{
+                    }).then((res) => {
+                        res.json().then((data) => {
                             console.log(data);
+                            if (data.error) {
+                                bot.sendMessage(chatID, data.error);
+                                return;
+                            }
+                            else {
+                                bot.sendMessage(chatID, "Newsletter sent successfully!");
+                            }
                         });
 
-                        bot.sendMessage(chatID, "Newsletter sent successfully!");
-                    }).catch((e)=>{
+
+                    }).catch((e) => {
                         console.log(e);
                         bot.sendMessage(chatID, "An error occured while sending the newsletter. Please try again later.");
                     });
@@ -100,4 +122,35 @@ bot.on('message', (msg) => {
 
 function checkAdmin(username) {
     return admins.includes(username);
+}
+
+async function getToken(username) {
+    
+    let data = JSON.stringify({
+        "encrypted_data": enc_data[username]
+    });
+
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://www.trustherole.in/ttrapi/user/auth/authenticate',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+    const resp  = await axios.request(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+            return response.data;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    if(resp && resp.token){
+        return resp.token;
+    }
+    return null;
 }
